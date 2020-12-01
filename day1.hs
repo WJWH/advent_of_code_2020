4daf911c828906b1956cbc02e0b28d7d35b41b13
Wander Hillen <wjw.hillen@gmail.com>
2020-12-01 13:18:40 +0100

    Replace Set with IntSet

--- day1.hs
+++ day1.hs
@@ -1,8 +1,8 @@
-import qualified Data.Set as S
+import qualified Data.IntSet as S
 
 -- Nice O(N) solution
 part1 = do
-  nums <- (foldr S.insert S.empty . map read . lines <$> readFile "day1_input.txt") :: IO (S.Set Int)
+  nums <- (foldr S.insert S.empty . map read . lines <$> readFile "day1_input.txt") :: IO (S.IntSet)
   let filteredSet = S.filter (\n -> (2020 - n) `S.member` nums) nums
   print . product . S.toList $ filteredSet
 
@@ -11,4 +11,4 @@ part2 = do
   nums <- map read . lines <$> readFile "day1_input.txt" :: IO [Int]
   print [ x*y*z | x <- nums, y <- nums, z <- nums, x < y, y < z, x+y+z == 2020]
 
--- $> part2
+-- $> part1 >> part2
