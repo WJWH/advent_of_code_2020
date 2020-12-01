d0d616963bb5f4b89bab17924e6d0b4833744cb3
Wander Hillen <wjw.hillen@gmail.com>
2020-12-01 12:10:50 +0100

    Add day 1 solutions

---
+++ day1.hs
@@ -0,0 +1,14 @@
+import qualified Data.Set as S
+
+-- Nice O(N) solution
+part1 = do
+  nums <- (foldr S.insert S.empty . map read . lines <$> readFile "day1_input.txt") :: IO (S.Set Int)
+  let filteredSet = S.filter (\n -> (2020 - n) `S.member` nums) nums
+  print . product . S.toList $ filteredSet
+
+-- Brute force but eh there's only 200 lines in the input
+part2 = do
+  nums <- map read . lines <$> readFile "day1_input.txt" :: IO [Int]
+  print [ x*y*z | x <- nums, y <- nums, z <- nums, x < y, y < z, x+y+z == 2020]
+
+-- $> part2
